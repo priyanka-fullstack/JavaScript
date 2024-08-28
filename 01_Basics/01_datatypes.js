46aea05e4478391295fdec8d072329ce799bcc24
Priyanka <137176659+priyanka-fullstack@users.noreply.github.com>
2024-08-28 12:50:41 +0000

    datatypes

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all js code as newer version 
+
+//premitive data types
+let fname = "Priyanka"; //string
+let age = 30;  //Number
+let isLoggedIn = true;  //boolean
+let state;  //undefined
+// null => standalone value;
+// symbol
+
+//non primitive 
+//object
+//typeOf null is object // null is an object
+console.log(typeof(undefined));
+console.log(typeof(null));
+console.log(typeof(fname));
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 345678;
+let accountEmail = "priyanka@gmail.com";
+var acoountPassword = "abc12234";
+accountCity = "Delhi";
+
+//accountId = 2;
+
+accountEmail = "pk00@gmail.com"
+acoountPassword = "bsabdqj"
+accountCity = "Jaipur"
+
+/*
+  Prefer not to use var
+  because of issue in block scope and functional scope.
+*/ 
+
+console.log(accountId);
+console.table([accountEmail,acoountPassword,accountCity]);
